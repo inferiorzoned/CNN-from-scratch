c58d64bb0958a71bce6fff91d262635edc2b7e5b
inferiorzoned <hasibul.hisham@newscred.com>
2023-06-16 04:26:35 +0600

    add documentation

--- cnn.py
+++ cnn.py
@@ -161,6 +161,55 @@ class ConvLayer:
         #         output[:, i, j, :] = self.convolve_single_pass(X_padded[:, i * self.stride:i * self.stride + self.filter_dim, j * self.stride:j * self.stride + self.filter_dim, :], W) + b
         
         return output  
+    
+    def backward(self, dZ, cache):
+        """
+        Performs the backward pass of the convolutional layer.
+        :param dZ: Gradient of the loss with respect to the output of the convolutional layer (Z), shape = (batch_size, height, width, num_channels)
+        :param cache: A tuple of values needed for the backward pass (X, W)
+        :return: Gradient of the loss with respect to the input (dX), shape = (batch_size, height, width, num_channels)
+        """
+        X, W = cache
+        batch_size, height_X, width_X, num_channels_X = X.shape
+        filter_dim, filter_dim, num_channels_X, num_channels_W = W.shape
+
+        dX = np.zeros(X.shape)
+        dW = np.zeros(W.shape)
+        db = np.zeros((num_channels_W))
+
+        # Pad X and dX
+        X_padded = self.pad_input(X)
+        dX_padded = self.pad_input(dX)
+
+        # db
+        for n in range(num_channels_W):
+            db[n] += np.sum(dZ[:, :, :, n])
+
+        # dW
+        for i in range(filter_dim):
+            for j in range(filter_dim):
+                for k in range(num_channels_X):
+                    for l in range(num_channels_W):
+                        x_section = X_padded[:, i:i+height_X*self.stride:self.stride, j:j+width_X*self.stride:self.stride, k]
+                        dZ_section = dZ[:, :, :, l]
+                        dW[i, j, k, l] += np.sum(x_section * dZ_section)
+
+        # dX
+        for i in range(height_X):
+            for j in range(width_X):
+                for k in range(num_channels_X):
+                    for l in range(num_channels_W):
+                        w_section = W[:, :, k, l]
+                        w_section_rotated = np.rot90(np.rot90(w_section))
+                        dZ_section = dZ[:, i:i+self.stride*self.filter_dim:self.stride, j:j+self.stride*self.filter_dim:self.stride, l]
+                        dX_padded[:, i:i+self.filter_dim*self.stride:self.stride, j:j+self.filter_dim*self.stride:self.stride, k] += w_section_rotated[np.newaxis,:,:] * dZ_section[:,:,:,np.newaxis]
+
+        # Unpad dX
+        if self.padding != 0:
+            dX = dX_padded[:, self.padding:-self.padding, self.padding:-self.padding,:]
+
+        return dX
+    
     def __str__(self) -> str:
         return "ConvLayer(num_channels={}, filter_dim={}, stride={}, padding={})".format(self.num_channels, self.filter_dim, self.stride, self.padding)
         
@@ -209,6 +258,26 @@ class MaxPoolingLayer:
                 output[:, int(np.floor(i / self.stride)), int(np.floor(j / self.stride)), :] = np.max(
                     X[:, i:i + self.filter_dim, j:j + self.filter_dim, :], axis=None)
         return output
+    
+    def backward(self, dZ, cache):
+        """
+        Performs the backward pass of the max pooling layer.
+        :param dZ: Gradient of the loss with respect to the output of the max pooling layer (Z), shape = (batch_size, height, width, num_filters)
+        :param cache: A tuple of values needed for the backward pass (X)
+        :return: Gradient of the loss with respect to the input (dX), shape = (batch_size, height, width, num_filters)
+        """
+        X = cache
+        batch_size, height_X, width_X, num_channels_X = X.shape
+
+        dX = np.zeros(X.shape)
+
+        for i in range(0, height_X - self.filter_dim + 1, self.stride):
+            for j in range(0, width_X - self.filter_dim + 1, self.stride):
+                x_section = X[:, i:i + self.filter_dim, j:j + self.filter_dim, :]
+                mask = (x_section == np.max(x_section, axis=(1, 2))[:, np.newaxis, np.newaxis])
+                dX[:, i:i + self.filter_dim, j:j + self.filter_dim] += mask * dZ[:, int(np.floor(i / self.stride)), int(np.floor(j / self.stride))][:,:,np.newaxis,np.newaxis]
+
+        return dX
 
     def __str__(self) -> str:
         return "MaxPoolingLayer(filter_dim={}, stride={})".format(self.filter_dim, self.stride)
@@ -231,6 +300,22 @@ class FullyConnectedLayer:
         b = np.zeros(self.output_dim)
 
         return np.dot(X, W) + b
+    
+    def backward(self, dZ, cache):
+        """
+        Performs the backward pass of the fully connected layer.
+        :param dZ: Gradient of the loss with respect to the output of the fully connected layer (Z), shape = (batch_size, output_dim)
+        :param cache: A tuple of values needed for the backward pass (X, W)
+        :return: Gradient of the loss with respect to the input (dX), shape = (batch_size, num_inputs)
+        """
+        X, W = cache
+        batch_size = X.shape[0]
+
+        dX = np.dot(dZ, W.T)
+        dW = np.dot(X.T, dZ)
+        db = np.sum(dZ, axis=0)
+
+        return dX
 
     def __str__(self) -> str:
         return "FullyConnectedLayer(output_dim={})".format(self.output_dim)
@@ -285,14 +370,21 @@ class FlattenLayer:
         """
         # return X.reshape(1, -1)
         return X.reshape(X.shape[0], -1)
-    
-    # def backward(self, X, dL_dout):
-    #     """
-    #     :param X: input data, shape = (1, num_inputs)
-    #     :param dL_dout: gradient of loss with respect to output of this layer, shape = (1, num_inputs)
-    #     :return: gradient of loss with respect to input of this layer, shape = (1, num_inputs)
-    #     """
-    #     return dL_dout.reshape(X.shape)
+        
+    def backward(self, dZ, cache):
+        """
+        Performs the backward pass of the softmax layer.
+        :param dZ: Gradient of the loss with respect to the output of the softmax layer (Z), shape = (batch_size, num_classes)
+        :param cache: A tuple of values needed for the backward pass (X)
+        :return: Gradient of the loss with respect to the input (dX), shape = (batch_size, num_classes)
+        """
+        X = cache
+        batch_size = X.shape[0]
+
+        dX = dZ.copy()
+        dX -= np.sum(dZ * X, axis=1)[:, np.newaxis] * X
+
+        return dX
     
     def __str__(self) -> str:
         return "FlattenLayer()"
@@ -399,6 +491,11 @@ def forward_pass(X, layers):
         X = layer.forward(X)
     return X
 
+def backward_pass(dZ, layers):
+    for layer in reversed(layers):
+        dZ = layer.backward(dZ)
+    return dZ
+
 # main function
 if __name__ == '__main__':
     # load mnist data
@@ -408,25 +505,31 @@ if __name__ == '__main__':
     layers = build_model()
     
     # train model for first batch
-    batch_x, batch_y = batches[0]
-    for layer in layers:
-        print("input shape: ", batch_x.shape)
-        print_g(layer)
-        batch_x = layer.forward(batch_x)
-        print_m(f"output shape: {batch_x.shape}")
-    y_hat = batch_x
-    print(y_hat)
+    # batch_x, batch_y = batches[0]
+    # for layer in layers:
+    #     print("input shape: ", batch_x.shape)
+    #     print_g(layer)
+    #     batch_x = layer.forward(batch_x)
+    #     print_m(f"output shape: {batch_x.shape}")
+    # y_hat = batch_x
+    # print(y_hat)
     # print_m(batch_y)
     # print(cross_entropy_loss(batch_y, y_hat))
     
+    num_epochs = 10
+    # train model
+    for epoch in range(num_epochs):
+        for batch in batches:
+            X, y = batch
+            y_hat = forward_pass(X, layers)
+            y_new = np.zeros_like(y_hat)
+            y_new[np.arange(len(y_hat)), y] = 1
+            loss = cross_entropy_loss(y_new, y_hat)
+            print(loss)
 
-    # # train model
-    # for batch in batches:
-    #     X, y = batch
-    #     y_hat = forward_pass(X, layers)
-        # y_new = 
-            # run backward
-            # dL_dout = layer.backward(X, y_hat)
+            # backpropagation
+            dZ = y_hat - y_new
+            backward_pass(dZ, layers)
         
 
 
